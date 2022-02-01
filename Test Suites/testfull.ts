0b46dd953dae8a83af61801b222d66cb2d435ee2
YARIB <YARIB@192.168.1.21>
2022-02-01 09:36:07 -0400

    Descripcion de los test case.

---
+++ Test Suites/testfull.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testfull</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cdf61194-df34-41fe-9d6f-a8bed533ae36</testSuiteGuid>
+   <testCaseLink>
+      <guid>5db40877-9bb0-49c2-a250-01bfd35e9a8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ejercicio1/test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1900ed4b-37e5-4048-8ccc-f13649fa763c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ejercicio1/test2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7888675-00c5-4aab-9923-e4b470086ed6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ejercicio1/test3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fbf6f61b-cab7-45a9-9dcc-b5e55b0f5e06</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ejercicio1/test4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6a9288a-24c6-449e-abdf-45d765aeea24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ejercicio1/test5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
